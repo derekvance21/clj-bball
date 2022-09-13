882f4c3dc0035e4a9d44c89f41d287f5f66226dd
Derek Vance <derekvance21@gmail.com>
2022-09-12 21:17:00 -0700

    query offensive rating, other useful items

--- src/core.clj
+++ src/core.clj
@@ -22,13 +22,33 @@
                             :db/doc "the order this action happened in its possession"}
              :action/free-throw {:db/valueType :db.type/boolean
                                  :db/cardinality :db.cardinality/many
-                                 :db/doc "the free throw attempts of this action"}
+                                 :schema/see-instead :action/ft-made
+                                 :db/doc "DEPRECATED, SEE :action/ft-made and :action/ft-attempted | the free throw attempts of this action"}
+             :action/ft-1 {:db/valueType :db.type/boolean
+                           :db/cardinality :db.cardinality/one
+                           :schema/see-instead :action/ft-made
+                           :db/doc "DEPRECATED, SEE :action/ft-made and :action/ft-attempted | the result of the first free throw attempt"}
+             :action/ft-2 {:db/valueType :db.type/boolean
+                           :db/cardinality :db.cardinality/one
+                           :schema/see-instead :action/ft-made
+                           :db/doc "DEPRECATED, SEE :action/ft-made and :action/ft-attempted | the result of the second free throw attempt"}
+             :action/ft-3 {:db/valueType :db.type/boolean
+                           :db/cardinality :db.cardinality/one
+                           :schema/see-instead :action/ft-made
+                           :db/doc "DEPRECATED, SEE :action/ft-made and :action/ft-attempted | the result of the third free throw attempt"}
+             :action/ft-made {:db/valueType :db.type/long
+                              :db/cardinality :db.cardinality/one
+                              :db/doc "the number of made free throws in this action"}
+             :action/ft-attempted {:db/valueType :db.type/long
+                                   :db/cardinality :db.cardinality/one
+                                   :db/doc "the number of attempted free throws in this action"}
              :shot/distance {:db/valueType :db.type/long
                              :db/cardinality :db.cardinality/one
                              :db.attr/preds 'src.core/valid-shot-distance?
                              :db/doc "the distance from the hoop in feet the shot was attempted from"}
              :possession/action {:db/valueType :db.type/ref
                                  :db/cardinality :db.cardinality/many
+                                 :db/isComponent true
                                  :db/doc "the actions of this possession"}
              :possession/lineup {:db/valueType :db.type/ref
                                  :db/cardinality :db.cardinality/many
@@ -47,6 +67,7 @@
                                 :db/doc "the order this possession happened in its game"}
              :game/possession {:db/valueType :db.type/ref
                                :db/cardinality :db.cardinality/many
+                               :db/isComponent true
                                :db/doc "the possessions of this game"}
              :game/minutes {:db/valueType :db.type/long
                             :db/cardinality :db.cardinality/one
@@ -91,28 +112,28 @@
      @conn)
 
 (def game [{:db/id -1
-             :team/name "Blaine Borderites"}
-            {:db/id -2
-             :team/name "Bellingham Bayhawks"}
-            {:game/minutes 32
-             :game/possession [{:possession/team -1
-                                :possession/order 1
-                                :possession/points 2
-                                :possession/action [{:action/order 1
-                                                     :action/type :action.type/miss-3
-                                                     :action/player {:player/number 10
-                                                                     :player/team -1}}
-                                                    {:action/order 2
-                                                     :action/type :action.type/make-2
-                                                     :action/player {:player/number 22
-                                                                     :player/team -1}}]}
-                               {:possession/team -2
-                                :possession/order 1
-                                :possession/points 0
-                                :possession/action [{:action/order 1
-                                                     :action/type :action.type/miss-2
-                                                     :action/player {:player/number 33
-                                                                     :player/team -2}}]}]}])
+            :team/name "Blaine Borderites"}
+           {:db/id -2
+            :team/name "Bellingham Bayhawks"}
+           {:game/minutes 32
+            :game/possession [{:possession/team -1
+                               :possession/order 1
+                               :possession/points 2
+                               :possession/action [{:action/order 1
+                                                    :action/type :action.type/miss-3
+                                                    :action/player {:player/number 10
+                                                                    :player/team -1}}
+                                                   {:action/order 2
+                                                    :action/type :action.type/make-2
+                                                    :action/player {:player/number 22
+                                                                    :player/team -1}}]}
+                              {:possession/team -2
+                               :possession/order 1
+                               :possession/points 0
+                               :possession/action [{:action/order 1
+                                                    :action/type :action.type/miss-2
+                                                    :action/player {:player/number 33
+                                                                    :player/team -2}}]}]}])
 
 (d/transact! conn game)
 
@@ -122,10 +143,16 @@
                     [?p :player/team ?t]])
 (d/q player-query @conn)
 
+; get all teams
+(d/q '[:find (pull ?t [*])
+       :where
+       [?t :team/name]]
+     @conn)
+
 (def three-point-attempt-rules '[[(three-point-attempt ?a)
-                                 [?a :action/type :action.type/make-3]]
-                                [(three-point-attempt ?a)
-                                 [?a :action/type :action.type/miss-3]]])
+                                  [?a :action/type :action.type/make-3]]
+                                 [(three-point-attempt ?a)
+                                  [?a :action/type :action.type/miss-3]]])
 
 ; gets all actions that were a three point attempt, and their possession id
 (def three-point-action-query '[:find ?p (pull ?a [:db/id :action/order :action/type :action/player :action/free-throw])
@@ -135,13 +162,150 @@
                                 (three-point-attempt ?a)])
 (d/q three-point-action-query @conn three-point-attempt-rules)
 
+; get all possessions
+(d/q '[:find (pull ?p [:db/id {:possession/team [:team/name]}])
+       :where
+       [?p :possession/team]]
+     @conn)
+
 ; gets all action ids for each possession, as collections for each possession
-(def possession-query '[:find (pull ?p [:db/id :possession/action])
-                        :where
-                        [?p :possession/order]])
-(d/q possession-query @conn)
+(d/q '[:find (pull ?p [:db/id
+                       {:possession/team [:db/id :team/name]}
+                       {:possession/action [* {:action/type [:db/ident]}]}])
+       :where
+       [?p :possession/order]]
+     @conn)
+;; => ([{:db/id 13,
+;;       :possession/team {:db/id 10, :team/name "Blaine Borderites"},
+;;       :possession/action
+;;       [{:db/id 14, :action/player #:db{:id 15}, :action/order 1, :action/type #:db{:ident :action.type/miss-3}}
+;;        {:db/id 16,
+;;         :action/player #:db{:id 17},
+;;         :action/order 2,
+;;         :action/type #:db{:ident :action.type/make-2},
+;;         :action/ft-1 true}]}]
+;;     [{:db/id 24,
+;;       :possession/team {:db/id 11, :team/name "Bellingham Bayhawks"},
+;;       :possession/action
+;;       [{:db/id 25,
+;;         :action/player #:db{:id 26},
+;;         :action/order 1,
+;;         :action/type #:db{:ident :action.type/bonus},
+;;         :action/ft-2 false,
+;;         :action/ft-1 true}]}]
+;;     [{:db/id 27,
+;;       :possession/team {:db/id 10, :team/name "Blaine Borderites"},
+;;       :possession/action
+;;       [{:db/id 28,
+;;         :action/player #:db{:id 29},
+;;         :action/order 1,
+;;         :action/type #:db{:ident :action.type/miss-3},
+;;         :action/ft-attempted 3,
+;;         :action/ft-made 2}]}]
+;;     [{:db/id 30,
+;;       :possession/team {:db/id 10, :team/name "Blaine Borderites"},
+;;       :possession/action
+;;       [{:db/id 31,
+;;         :action/player #:db{:id 32},
+;;         :action/order 1,
+;;         :action/type #:db{:ident :action.type/make-2},
+;;         :action/ft-attempted 1,
+;;         :action/ft-made 1}]}]
+;;     [{:db/id 18,
+;;       :possession/team {:db/id 11, :team/name "Bellingham Bayhawks"},
+;;       :possession/action
+;;       [{:db/id 19, :action/player #:db{:id 20}, :action/order 1, :action/type #:db{:ident :action.type/miss-2}}]}])
+
+; add an "and-one" to the specified action
+(d/transact! conn [{:db/id 16
+                    :action/ft-1 true}])
+
+(d/transact! conn [{:possession/order 3
+                    :possession/points 2
+                    :possession/team [:team/name "Blaine Borderites"]
+                    :possession/action [{:action/order 1
+                                         :action/player {:player/number 12
+                                                         :player/team [:team/name "Blaine Borderites"]}
+                                         :action/type :action.type/miss-3
+                                         :action/ft-attempted 3
+                                         :action/ft-made 2}]}])
+
+(d/transact! conn [{:possession/order 4
+                    :possession/points 2
+                    :possession/team [:team/name "Blaine Borderites"]
+                    :possession/action [{:action/order 1
+                                         :action/player {:player/number 12
+                                                         :player/team [:team/name "Blaine Borderites"]}
+                                         :action/type :action.type/make-2
+                                         :action/ft-attempted 1
+                                         :action/ft-made 1}]}])
 
 
+(d/q '[:find ?points
+       :in $ ?t
+       :where
+       [?p :possession/team ?t]
+       [?p :possession/action ?a]
+       [?a :action/type ?at]
+       [?at :db/ident ?type]
+       [(get-else $ ?a :action/ft-1 false) ?ft-1]
+       [(get-else $ ?a :action/ft-2 false) ?ft-2]
+       [(get-else $ ?a :action/ft-3 false) ?ft-3]
+       [(src.core/action->points ?type ?ft-1 ?ft-2 ?ft-3) ?points]]
+     @conn (src.core/team-name->eid "Blaine Borderites"))
+
+; get the number of points from FGs, FTs, and total possessions
+(d/q '[:find [(sum ?points) (sum ?ft-points) (count-distinct ?p)]
+       :in $ ?t
+       :where
+       [?p :possession/team ?t]
+       [?p :possession/action ?a]
+       [?a :action/type ?at]
+       [(get-else $ ?at :action/score 0) ?points]
+       [(get-else $ ?a :action/ft-made 0) ?ft-points]]
+     @conn (src.core/team-name->eid "Bellingham Bayhawks"))
+
+(defn team-offensive-rating
+  [team]
+  (let [[pts ftpts npos] (d/q '[:find [(sum ?points) (sum ?ft-points) (count-distinct ?p)]
+                                 :in $ ?t
+                                 :where
+                                 [?p :possession/team ?t]
+                                 [?p :possession/action ?a]
+                                 [?a :action/type ?at]
+                                 [(get-else $ ?at :action/score 0) ?points]
+                                 [(get-else $ ?a :action/ft-made 0) ?ft-points]]
+                               @conn (src.core/team-name->eid team))]
+    (/ (+ pts ftpts) npos)))
+
+(team-offensive-rating "Blaine Borderites")
+
+(defn ft-points
+  [ft]
+  (if ft 1 0))
+
+(if 0 4 2)
+
+(defn action->points
+  [type ft-1 ft-2 ft-3]
+  (+ (src.core/action-type->points type)
+     (src.core/ft-points ft-1)
+     (src.core/ft-points ft-2)
+     (src.core/ft-points ft-3)))
+
+(defn action-type->points
+  [type]
+  (case type
+    :action.type/make-2 2
+    :action.type/make-3 3
+    0))
+
+(defn team-name->eid
+  [team]
+  (d/q '[:find ?e .
+         :in $ ?team
+         :where
+         [?e :team/name ?team]] @conn team))
 
 ; functions for resetting and closing database
 (defn clear [] (d/clear conn))
