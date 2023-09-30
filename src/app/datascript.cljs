ad4f131e3a289365b0fda75c8803b03744cda5f7
Derek Vance <derekvance21@gmail.com>
2023-09-30 13:01:18 -0700

    add Blaine vs. Overlake 2023-03-02, other bug fixes

--- src/app/datascript.cljs
+++ src/app/datascript.cljs
@@ -15,23 +15,23 @@
 
 ;; datascript built-ins
 #_(def query-fns {'= =, '== ==, 'not= not=, '!= not=,
-                '< less, '> greater, '<= less-equal, '>= greater-equal,
-                '+ +, '- -, '* *, '/ /,
-                'quot quot, 'rem rem, 'mod mod, 'inc inc, 'dec dec, 'max max, 'min min,
-                'zero? zero?, 'pos? pos?, 'neg? neg?, 'even? even?, 'odd? odd?, 'compare compare,
-                'rand rand, 'rand-int rand-int,
-                'true? true?, 'false? false?, 'nil? nil?, 'some? some?, 'not not, 'and and-fn, 'or or-fn,
-                'complement complement, 'identical? identical?,
-                'identity identity, 'keyword keyword, 'meta meta, 'name name, 'namespace namespace, 'type type,
-                'vector vector, 'list list, 'set set, 'hash-map hash-map, 'array-map array-map,
-                'count count, 'range range, 'not-empty not-empty, 'empty? empty?, 'contains? contains?,
-                'str str, 'subs, subs, 'get get,
-                'pr-str pr-str, 'print-str print-str, 'println-str println-str, 'prn-str prn-str,
-                're-find re-find, 're-matches re-matches, 're-seq re-seq, 're-pattern re-pattern,
-                '-differ? -differ?, 'get-else -get-else, 'get-some -get-some, 'missing? -missing?, 'ground identity,
-                'clojure.string/blank? str/blank?, 'clojure.string/includes? str/includes?,
-                'clojure.string/starts-with? str/starts-with?, 'clojure.string/ends-with? str/ends-with?
-                'tuple vector, 'untuple identity})
+                  '< less, '> greater, '<= less-equal, '>= greater-equal,
+                  '+ +, '- -, '* *, '/ /,
+                  'quot quot, 'rem rem, 'mod mod, 'inc inc, 'dec dec, 'max max, 'min min,
+                  'zero? zero?, 'pos? pos?, 'neg? neg?, 'even? even?, 'odd? odd?, 'compare compare,
+                  'rand rand, 'rand-int rand-int,
+                  'true? true?, 'false? false?, 'nil? nil?, 'some? some?, 'not not, 'and and-fn, 'or or-fn,
+                  'complement complement, 'identical? identical?,
+                  'identity identity, 'keyword keyword, 'meta meta, 'name name, 'namespace namespace, 'type type,
+                  'vector vector, 'list list, 'set set, 'hash-map hash-map, 'array-map array-map,
+                  'count count, 'range range, 'not-empty not-empty, 'empty? empty?, 'contains? contains?,
+                  'str str, 'subs, subs, 'get get,
+                  'pr-str pr-str, 'print-str print-str, 'println-str println-str, 'prn-str prn-str,
+                  're-find re-find, 're-matches re-matches, 're-seq re-seq, 're-pattern re-pattern,
+                  '-differ? -differ?, 'get-else -get-else, 'get-some -get-some, 'missing? -missing?, 'ground identity,
+                  'clojure.string/blank? str/blank?, 'clojure.string/includes? str/includes?,
+                  'clojure.string/starts-with? str/starts-with?, 'clojure.string/ends-with? str/ends-with?
+                  'tuple vector, 'untuple identity})
 
 
 (def schema (schema/datomic->datascript-schema schema/schema))
@@ -879,6 +879,7 @@
        (.click))
      (.revokeObjectURL js/URL url))))
 
+
 (comment
   (let [filename "2022-12-06-Blaine-Bellingham.edn"
         game-id (get @re-frame.db/app-db :game-id)
@@ -889,6 +890,7 @@
   (clear-ls-game)
   ;
   )
+
 
 (comment
 
