ad74c54427e86202b51492f065f0f662ae30f545
Derek Vance <derekvance21@gmail.com>
2023-10-25 18:45:07 -0700

    simplify core.clj

--- src/bball/core.clj
+++ src/bball/core.clj
@@ -1,15 +1,12 @@
 (ns bball.core
   (:require
    [clojure.java.io :as io]
-   [datomic.api :as datomic]
-   [bball.db :as db]
    [bball.env :as env]
    [clojure.edn :as edn]
    [ring.adapter.jetty :as jetty]
    [ring.middleware.defaults :as defaults]
-   [ring.util.request :as request]
    [ring.util.response :as response]
-   [compojure.core :refer [defroutes GET POST]]
+   [compojure.core :refer [defroutes GET]]
    [compojure.route :as route])
   (:gen-class))
 
@@ -33,7 +30,6 @@
 
 
 (defroutes app-routes
-  (GET "/" [] "<html><head><title>clj-bball</title></head><body><div style=\"height: 90vh; display: flex; flex-direction: column; justify-content: center; align-items: center\"><p style=\"font-family: sans-serif; \">Hello, Ring!</p></div></body></html>")
   (route/files "/" {:root "resources/public"})
   (wrap-cors
    (GET "/games" [] (response/response
@@ -42,15 +38,6 @@
                      (pr-str
                       ;; this might be able to be just `map`
                       (mapv parse-edn (folder-files "resources/games"))))))
-  (GET "/db" [] {:headers {"Access-Control-Allow-Origin" "*"
-                           "Content-Type" "application/edn"}
-                 :body (pr-str (db/datomic->datascript-db (db/get-db)))})
-  (POST "/db" [] (fn [req]
-                   (let [tx-data (edn/read-string (request/body-string req))
-                         tx-result (deref (datomic/transact (db/get-connection) tx-data))
-                         db-after (:db-after tx-result)]
-                     {:body (pr-str (db/datomic->datascript-db db-after))
-                      :headers {"Access-Control-Allow-Origin" "*"}})))
   (route/not-found "Not Found"))
 
 
@@ -69,7 +56,7 @@
 (defn -main [& args]
   (let [opts (apply hash-map (map edn/read-string args))
         {:keys [dev]} opts]
-    (println (str "Serving app at port " (parse-long (env/env :APP_SERVER_PORT "8900")) (when dev " [DEV]")))
+    (println (str "Serving app at port http://localhost:" (parse-long (env/env :APP_SERVER_PORT "8900")) (when dev " [DEV]")))
     (start)))
 
 
